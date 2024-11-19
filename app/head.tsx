e2524051eb397ca60a3d7dbac75db41275b4e01a
ELATTAR-Ayoub <elattarayoub000@gmail.com>
2024-11-19 14:59:23 +0100

    update head

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Yukirhythm </title>
+      <title>iPOS 365 </title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta
-        name="description"
-        content="Yukirhythm a music app that searches for whatever your looking for by scraping the web."
-      />
+      <meta name="description" content="iPOS 365." />
       <link
         rel="icon"
         href="/icon?<generated>"
